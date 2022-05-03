3bbe03aedef60ab21d75591aa1699e2c1ed2db9f
jin-y <854410806@qq.com>
2022-05-04 07:03:31 +0800

    2-8 增加test接口 访问地址127.0.0.1：8080/hello/post

--- demo/src/main/java/com/wiki/demo/controller/TestController.java
+++ demo/src/main/java/com/wiki/demo/controller/TestController.java
@@ -1,17 +1,19 @@
 package com.wiki.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
 //    @GetMapping("/hello")
-    @RequestMapping("hello")
+    @RequestMapping("/hello")
     public String hello(){
         return "aaa";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return name + "post";
     }
 }
