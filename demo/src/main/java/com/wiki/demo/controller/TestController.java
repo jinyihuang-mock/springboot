344ea4b4eaec420c6fa55fa1638647a4a0b26e20
jin-y <854410806@qq.com>
2022-05-05 07:37:44 +0800

    3-4 集成mybatis

--- demo/src/main/java/com/wiki/demo/controller/TestController.java
+++ demo/src/main/java/com/wiki/demo/controller/TestController.java
@@ -1,22 +1,31 @@
 package com.wiki.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Component
 @RestController
+@PropertySource(value = "classpath:application.properties")
 public class TestController {
 
     //测试
-    @Value("$test.hello:Test")
-    private String testHello;
+    @Value("$test")
+//    @Autowired
+    private String test;
 
 //    @GetMapping("/hello")
+
+
     @RequestMapping("/hello")
     public String hello(){
-        return "aaa"+testHello;
+        return "aaa"+test;
     }
+
+
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return name + "post";
