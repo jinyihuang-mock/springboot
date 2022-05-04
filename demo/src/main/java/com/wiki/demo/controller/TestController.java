aa995f6e730a00db99ea0486889f831449655282
jin-y <854410806@qq.com>
2022-05-04 14:52:02 +0800

    2-9 Spring 配置文件 访问地址127.0.0.1：8880/hello

--- demo/src/main/java/com/wiki/demo/controller/TestController.java
+++ demo/src/main/java/com/wiki/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wiki.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    //测试
+    @Value("$test.hello:Test")
+    private String testHello;
+
 //    @GetMapping("/hello")
     @RequestMapping("/hello")
     public String hello(){
-        return "aaa";
+        return "aaa"+testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
