d8a40dfa40053ccfb1b206a0be1cf7bc6fa395fc
jin-y <854410806@qq.com>
2022-05-16 18:51:45 +0800

    3-7 集成mybatis

--- demo/src/main/java/com/wiki/demo/mapper/TestMapper.java
+++ demo/src/main/java/com/wiki/demo/mapper/TestMapper.java
@@ -1,11 +1,30 @@
 package com.wiki.demo.mapper;
 
 import com.wiki.demo.domain.Test;
-import org.springframework.stereotype.Repository;
-
+import com.wiki.demo.domain.TestExample;
 import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
-@Repository
 public interface TestMapper {
-    public List<Test> list();
+    long countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    Test selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
 }
