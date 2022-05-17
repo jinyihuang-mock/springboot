8474dfaad9c8ca8cd2e3584900d33b74d97d50a1
jin-y <854410806@qq.com>
2022-05-17 18:39:45 +0800

    4-2 集成 ant design vue

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <a-button type="primary" danger ghost>Danger</a-button>
   </div>
 </template>
 
