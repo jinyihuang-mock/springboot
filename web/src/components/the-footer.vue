4d1e6685443f2e460a833a59f4e2a60622b27457
jin-y <854410806@qq.com>
2022-05-20 08:38:22 +0800

    4-5 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    个人电子书5.20
+  </a-layout-footer>
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
